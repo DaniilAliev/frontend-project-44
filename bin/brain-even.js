4eb6bbb88d60f15834135a10de3f64370a9f86a2
DaniilAliev <de.aliev@yandex.ru>
2022-12-08 14:38:42 +0300

    make brain-games fixed brake

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -12,25 +12,23 @@ const brainEven = () => {
   for (let i = 0; i < 3; i += 1) {
     arrRandom.push(Math.round(Math.random() * 100));
   }
-  console.log(arrRandom); // Контроль
-  let result = true;
-  do {
-    for (const number of arrRandom) {
-      console.log('Question:', number);
-      const answer = readlineSync.question('Your answer: ');
-      if (number % 2 === 0 && answer === 'no') {
-        console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-        console.log('Let\'s try again, Danya!');
-        result = false;
-      } else if (number % 2 !== 0 && answer === 'yes') {
-        console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-        console.log('Let\'s try again, Danya!');
-        result = false;
-      } else {
-        console.log('Correct!');
-      }
+  console.log(arrRandom); // Контроль массива
+
+  for (const number of arrRandom) {
+    console.log('Question:', number);
+    const answer = readlineSync.question('Your answer: ');
+    if (number % 2 === 0 && answer === 'no') {
+      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+      console.log('Let\'s try again, Danya!');
+      break;
+    } else if (number % 2 !== 0 && answer === 'yes') {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log('Let\'s try again, Danya!');
+      break;
+    } else {
+      console.log('Correct!');
     }
-  } while (result === true);
+  }
 };
 
 export default brainEven;
