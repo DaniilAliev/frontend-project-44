87cd6eedfdc802679a855cc84018c07d5b039a1e
DaniilAliev <de.aliev@yandex.ru>
2022-12-08 16:17:49 +0300

    make brain-games fixed

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-restricted-syntax */
 import readlineSync from 'readline-sync';
-import firstGame from '../src/cli.js';
 
 const brainEven = () => {
-  firstGame();
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
 
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
@@ -12,21 +13,24 @@ const brainEven = () => {
   for (let i = 0; i < 3; i += 1) {
     arrRandom.push(Math.round(Math.random() * 100));
   }
-  console.log(arrRandom); // Контроль массива
-
+  let i = 0;
   for (const number of arrRandom) {
     console.log('Question:', number);
     const answer = readlineSync.question('Your answer: ');
-    if (number % 2 === 0 && answer === 'no') {
+    if (number % 2 === 0 && answer !== 'yes') {
       console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-      console.log('Let\'s try again, Danya!');
+      console.log(`Let's try again, ${userName}!`);
       break;
-    } else if (number % 2 !== 0 && answer === 'yes') {
+    } else if (number % 2 !== 0 && answer !== 'no') {
       console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      console.log('Let\'s try again, Danya!');
+      console.log(`Let's try again, ${userName}!`);
       break;
     } else {
       console.log('Correct!');
+      i += 1;
+    }
+    if (i === 3) {
+      console.log(`Congratulations, ${userName}!`);
     }
   }
 };
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable import/extensions */
 
-import firstGame from '../src/cli.js';
+// import firstGame from '../src/cli.js';
 import brainEven from './brain-even.js';
 
 // firstGame();
